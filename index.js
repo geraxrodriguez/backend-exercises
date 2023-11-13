57fa70f4b159387617e02012239af68686d77905
Gerardo Rodriguez <gerarodriguez@proton.me>
2023-11-12 18:14:46 -0600

    completed 3.4

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ app.get('/api/persons/:id',(req, res)=>{
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    person = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
 const PORT = 3001   // declare port #
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
